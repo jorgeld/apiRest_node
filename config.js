0f88e2f480aa60359186c2a1c2d6f3393df90565
jlainez <jorgeld1986@gmail.com>
2017-11-06 15:32:55 +0100

    Conexión BBDD

--- config.js
+++ config.js
@@ -2,5 +2,3 @@ module.exports = {
     port : process.env.PORT || 3001,
     db : process.env.MONGODB || 'mongodb://admin:admin@ds147965.mlab.com:47965/filomax'
 }
-
-// db : process.env.MONGODB || 'mongodb://localhost:27017/shop'
