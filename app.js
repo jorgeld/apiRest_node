b3f255c440cef4a06185260ca6c79beff7091a90
jlainez <jorgeld1986@gmail.com>
2018-06-28 10:19:17 +0200

    Solucionado problema longitud llamada

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json());
 // app.use(bodyParser.json({limit: "50mb"}));
 // app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
