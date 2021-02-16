ac733b25007253e680e57bc659ff5a47ac05ae89
jlainez <jorgeld1986@gmail.com>
2021-02-16 12:32:53 +0100

    Mof db config

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
+// module.exports = {
+//     port : process.env.PORT || 3000,
+//     // db : process.env.MONGODB || 'mongodb://admin:admin@ds147965.mlab.com:47965/filomax',
+//     db : process.env.MONGODB || 'mongodb://admin:admin@cluster0.7tyxz.mongodb.net/test',
+// };
+
 module.exports = {
-    port : process.env.PORT || 3000,
-    db : process.env.MONGODB || 'mongodb://admin:admin@ds147965.mlab.com:47965/filomax'
+    PORT: process.env.PORT || 3000,
+    MONGODB_HOST: process.env.MONGODB_HOST || 'localhost',
+    MONGODB_DATABASE: process.env.MONGODB_DB || 'notes-app'
 };
--- index.js
+++ index.js
@@ -5,20 +5,32 @@ const app = require('./app');
 const config = require('./config');
 const port = process.env.PORT || 3002;
 
-// Conectamos a la bbdd
-mongoose.connect(config.db,function(err, res){
-    if(err){
-        return console.log(`Error al conectar a la BBDD: ${err}`)
-    }else{
-        console.log('Conexión a la Base de datos establecida');
-    }
+const MONGODB_URI = "mongodb+srv://admin:admin@cluster0.7tyxz.mongodb.net/bananas?retryWrites=true&w=majority";
 
-    app.on('Escuchando ---> ' , function(){
-        console.log('Servidor Funcionando ....')
-    });
+mongoose
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then((db) => console.log("Mongodb is connected to", db.connection.host))
+    .catch((err) => console.error(err));
 
-    app.listen(config.port,() => {
-        console.log(`API REST corriendo en http://localhost:${config.port}`);
-    });
-
-});
+// // Conectamos a la bbdd
+// mongoose.connect(config.db,function(err, res){
+//     if(err){
+//         return console.log(`Error al conectar a la BBDD: ${err}`)
+//     }else{
+//         console.log('Conexión a la Base de datos establecida');
+//     }
+//
+//     app.on('Escuchando ---> ' , function(){
+//         console.log('Servidor Funcionando ....')
+//     });
+//
+//     app.listen(config.port,() => {
+//         console.log(`API REST corriendo en http://localhost:${config.port}`);
+//     });
+//
+// });
